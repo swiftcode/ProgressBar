617f9e69cdedd94dc3f303408d7a0fb7a5a8bc38
swiftcode <swiftcode@users.noreply.github.com>
2024-01-25 20:44:16 -0600

    Initial Commit

---
+++ ProgressBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgressBar
+//
+//  Created by Michael Campbell on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
